e5511e252d985adc97b63fbe1d934a2d53631a4e
Daniel Rossman <drossman@athenahealth.com>
2019-11-06 15:12:38 -0500

    foo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [users, setUsers] = useState([
+    { id: 1, name: "Dan", email: "d@r.com" },
+    { id: 2, name: "Sarah", email: "s@k.com" },
+    { id: 3, name: "Jeff", email: "j@b.com" }
+  ]);
+
   const h1Style = {
     color: "red",
     marginBottom: 20
   };
 
-  const users = [
-    { id: 1, name: "Dan", email: "d@r.com" },
-    { id: 1, name: "Sarah", email: "s@k.com" },
-    { id: 1, name: "Jeff", email: "j@b.com" }
-  ];
+  function handleDelete(id) {
+    const newUsers = users.filter(user => user.id !== id);
+    setUsers(newUsers); // update State so react knows to rerender
+  }
 
   return (
     <>
       <h1 className="header" style={h1Style}>
-        App
+        users
       </h1>
+      <ul>
+        {users.map(user => (
+          <li>
+            <button onClick={() => handleDelete(user.id)}>Delete</button>
+            {user.name}
+          </li>
+        ))}
+      </ul>
       <label htmlFor="firstName">First name </label>
       <input id="firstName" type="text"></input>
       <p>My App.</p>
